e869ca512c4f250a370196021bb0d33aa0c0631b
ssarcandy <ssarcandy@gmail.com>
2017-05-06 03:33:35 +0800

    add proxy

---
+++ proxy.js
@@ -0,0 +1,19 @@
+var express = require('express')
+const request = require('request');
+const api = 'https://www.maicoin.com/api/prices/eth-twd';
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/eth-realtime', function (req, res) {
+    request(api, function (error, response, body) {
+        if (error) {
+            return;
+        }
+        res.send(body);
+
+    });
+})
+
+app.listen(3333)
