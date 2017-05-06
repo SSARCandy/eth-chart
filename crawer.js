99648b42ad74cd1163b6e1e9648a181957f916a8
ssarcandy <ssarcandy@gmail.com>
2017-05-06 22:11:52 +0800

    fix crawer

--- crawer.js
+++ crawer.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const fs = require('fs');
 const api = 'https://www.maicoin.com/api/prices/eth-twd';
-const history = require('./history.json');
+const history = require('./static/history.json');
 
 request(api, function (error, response, body) {
     if (error) {
@@ -16,5 +16,5 @@ request(api, function (error, response, body) {
         time: new Date()
     });
 
-    fs.writeFileSync(`${__dirname}/history.json`, JSON.stringify(history));
+    fs.writeFileSync(`${__dirname}/static/history.json`, JSON.stringify(history));
 });
