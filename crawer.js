71c369b413b45305603ec9917bb8905bef20aa4b
ssarcandy <ssarcandy@gmail.com>
2017-05-09 11:49:36 +0800

    add btc

--- crawer.js
+++ crawer.js
@@ -1,20 +1,37 @@
 const request = require('request');
 const fs = require('fs');
-const api = 'https://www.maicoin.com/api/prices/eth-twd';
-const history = require('./static/history.json');
+const api = 'https://www.maicoin.com/api/prices';
+const eth_history = require('./static/history.json');
+const btc_history = require('./static/btc_history.json');
 
-request(api, function (error, response, body) {
+request(`${api}/eth-twd`, function (error, response, body) {
     if (error) {
         return;
     }
 
     const { raw_sell_price, raw_buy_price } = JSON.parse(body);
 
-    history.push({
+    eth_history.push({
         buy: raw_buy_price,
         sell: raw_sell_price,
         time: new Date()
     });
 
-    fs.writeFileSync(`${__dirname}/static/history.json`, JSON.stringify(history));
+    fs.writeFileSync(`${__dirname}/static/history.json`, JSON.stringify(eth_history));
+});
+
+request(`${api}/btc-twd`, function (error, response, body) {
+    if (error) {
+        return;
+    }
+
+    const { raw_sell_price, raw_buy_price } = JSON.parse(body);
+
+    btc_history.push({
+        buy: raw_buy_price,
+        sell: raw_sell_price,
+        time: new Date()
+    });
+
+    fs.writeFileSync(`${__dirname}/static/btc_history.json`, JSON.stringify(btc_history));
 });
--- proxy.js
+++ proxy.js
@@ -1,13 +1,13 @@
 var express = require('express')
 const request = require('request');
-const api = 'https://www.maicoin.com/api/prices/eth-twd';
+const api = 'https://www.maicoin.com/api/prices';
 var cors = require('cors')
 var app = express()
 
 app.use(cors())
 
 app.get('/eth-realtime', function (req, res) {
-    request(api, function (error, response, body) {
+    request(`${api}/eth-twd`, function (error, response, body) {
         if (error) {
             return;
         }
@@ -16,6 +16,15 @@ app.get('/eth-realtime', function (req, res) {
     });
 })
 
+app.get('/btc-realtime', function (req, res) {
+    request(`${api}/btc-twd`, function (error, response, body) {
+        if (error) {
+            return;
+        }
+        res.send(body);
+
+    });
+})
 app.use(express.static('static'));
 
 app.listen(3333)
