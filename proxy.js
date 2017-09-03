acb832893e0c738572891c71e2ea28ac146bb641
ssarcandy <ssarcandy@gmail.com>
2017-09-03 21:08:27 +0800

    add api proxy

--- proxy.js
+++ proxy.js
@@ -1,6 +1,8 @@
 var express = require('express')
 const request = require('request');
 const api = 'https://www.maicoin.com/api/prices';
+const coinbase = 'https://api.coinbase.com/v2/prices/';
+
 var cors = require('cors')
 var app = express()
 
@@ -25,6 +27,26 @@ app.get('/btc-realtime', function (req, res) {
 
     });
 })
+
+app.get('/maicoin/:type', function (req, res) {
+    request(`${api}/${req.params.type.toLowerCase()}`, function (error, response, body) {
+        if (error) {
+            return;
+        }
+        res.send(body);
+
+    });
+})
+
+app.get('/coinbase/:type', function(req, res) {
+    request(`${coinbase}/${req.params.type.toUpperCase()}-USD/spot`, function (error, response, body) {
+        if (error) {
+            return;
+        }
+        res.send(body);
+    });
+})
+
 app.use(express.static('static'));
 
 app.listen(3333)
